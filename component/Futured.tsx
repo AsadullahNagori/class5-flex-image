599cf2f130c17affe3d7e6dbfadf5992a4bcb9d7
AsadullahNagori <164639593+AsadullahNagori@users.noreply.github.com>
2024-11-17 00:14:40 +0500

    Add files via upload

---
+++ component/Futured.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link"
+export default function Futured(){
+    return(
+  <div>
+   
+     
+      <div className="grid grid-cols-4 grid-rows-1 gap-2 m-4 h-60 ">
+  <div className="row-span-1 bg-red-500"> <img src="/download.jpeg" 
+  className="w-full h-60 object-cover">
+  </img></div>
+  <div className="row-span-1 bg-red-100"><img src="/flowered.jpg" 
+  className="w-full h-60 object-cover">
+  </img></div>
+  <div className="row-span-1 bg-red-500"><img src="/images (1).jpeg" 
+  className="w-full h-60 object-cover">
+  </img></div>
+  <div className="row-span-1 bg-red-500"><img src="/images.jpeg" 
+  className="w-full h-60 object-cover">
+  </img></div>
+         
+  </div></div>
+  
+  
+    );
+  }
+  
---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Futured from "../../component/Futured";
+
+export default function Home(){
+  return(
+<div>
+  <div className="text-8x1 text-red-700  font-extrabold text-center">
+    Asadullah</div>
+<Futured />
+
+    </div>
+  );
+}
+
+
